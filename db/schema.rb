a2b8a660b8d8b3527f3a87a04493fd377fdf45be
mineyuki-kobayashi <mine_e89@yahoo.co.jp>
2020-09-16 11:11:35 +0900

    データベースにユーザー情報を保存

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,16 @@ ActiveRecord::Schema.define(version: 2020_09_15_020317) do
     t.string "first_name_furigana", null: false
     t.string "last_name_furigana", null: false
     t.date "birthday", null: false
-    t.string "email", null: false
-    t.string "password", null: false
     t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
