b4d7ba51b3509295edc58d8f60630b23bc094dbb
mineyuki-kobayashi <mine_e89@yahoo.co.jp>
2020-09-16 19:23:21 +0900

    ログイン実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < ApplicationController
-
   def index
     
   end
+
+  def destroy
+    room = User.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :birthday, presence: true
-
-  VALID_NAME_REGEX =/\A[ァ-ヶー－]+\z/
-  validates :first_name_furigana, presence: true,format: { with: VALID_NAME_REGEX }
-  validates :last_name_furigana, presence: true,format: { with: VALID_NAME_REGEX }
-
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
-  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
+         validates :nickname, presence: true
+       
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :birthday, presence: true
+       
+         VALID_NAME_REGEX =/\A[ァ-ヶー－]+\z/
+         validates :first_name_furigana, presence: true,format: { with: VALID_NAME_REGEX }
+         validates :last_name_furigana, presence: true,format: { with: VALID_NAME_REGEX }
+       
+         VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+         validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
 
 end
