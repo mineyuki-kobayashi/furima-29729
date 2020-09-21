3bb03804d274b74c98cd3cba6e7272e587101713
mineyuki-kobayashi <mine_e89@yahoo.co.jp>
2020-09-21 19:35:56 +0900

    出品機能の選択肢のバリデーション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+    @articles = Item.order("created_at DESC")
   end
   
   def new
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,16 @@
 class Item < ApplicationRecord
   #アソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre
+  belongs_to_active_hash :category, :postage, :schedule, :source, :status
   belong_to :user
   #バリデーション
-  validates :user, :name, :explanation, :genre, presence: true
-  validates :genre_id, numericality: { other_than: 1 } 
+  validates :user, :name, :explanation, :category, :postage, :schedule, :source, :status, presence: true
+  
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :postage_id
+    validates :schedule_id
+    validates :source_id
+    validates :status_id
+  end
 end
