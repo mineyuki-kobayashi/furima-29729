5f10d2a9f17332f8999c4163166b36ab3c54850b
mineyuki-kobayashi <mine_e89@yahoo.co.jp>
2020-09-21 12:19:06 +0900

    Genreモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
-  #バリデーション入力
+  #アソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+  belong_to :user
+  #バリデーション
+  validates :user, :name, :explanation, :genre, presence: true
+  validates :genre_id, numericality: { other_than: 1 } 
 end
