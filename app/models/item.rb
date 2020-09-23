19921c42f5843d1a5c9715afabc6f99ccf8e2ddb
mineyuki-kobayashi <mine_e89@yahoo.co.jp>
2020-09-23 12:40:45 +0900

    各テーブルにアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :source
   belongs_to_active_hash :status
   belongs_to :user
+  has_one :list
   has_one_attached :image
   #バリデーション
   with_options presence: true do
