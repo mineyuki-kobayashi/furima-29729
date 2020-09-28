07d2b53f0190f6ccd5c8ca55cb25949783e15f80
mineyuki-kobayashi <mine_e89@yahoo.co.jp>
2020-09-28 17:32:42 +0900

    編集機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def edit
-  #  @item = Item.find(params[:id])
-  # end
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path(item.id)
+  end
 
   # def destroy
   # item = Item.find(params[:id])
