63ad5e112f175e552845bb18e18b2a4990b59c9c
mineyuki-kobayashi <mine_e89@yahoo.co.jp>
2020-09-28 18:52:27 +0900

    コードレビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,9 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
+    if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :new
