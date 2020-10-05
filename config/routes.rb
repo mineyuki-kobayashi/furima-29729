2fbc7f4b69cff56142f371a1f7066996176ac62a
mineyuki-kobayashi <mine_e89@yahoo.co.jp>
2020-10-05 17:39:48 +0900

    ルーティングネスト設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'purchases/index'
   devise_for :users
   root to: 'items#index'
   resources :items
+  resources :item do
+    resources :orders
+    resources :purchases
+  end
 end
